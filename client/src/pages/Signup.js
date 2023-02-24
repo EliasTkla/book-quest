5acb64e509bc1afb4a024a453779f330f6a97cc8
Elias <60025074+EliasTkla@users.noreply.github.com>
2023-02-23 19:22:14 -0500

    Update Signup.js

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -80,7 +80,7 @@ function Signup() {
         <input type="password" name="password" placeholder='Confirm your password' onChange={(e => setConfirmPwd(e.target.value))} /><br/>
         <h5>Already have an account? <Link to='/login'>Login Here</Link></h5>
         <button type='button' onClick={signUser}>Register</button>
-      </form>
+      </form><span id='login-space'>
     </div>
   )
 }
